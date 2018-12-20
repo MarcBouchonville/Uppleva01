7f596ed20241542b332c51e1fa95d92e981373aa
MarcBouchonville <marcbouchonville@gmail.com>
2018-12-20 16:07:10 +0100

    page03 : onmouseover + window.screen (availWidth et availHeight)

--- PhpProject1/01pages/page03.php
+++ PhpProject1/01pages/page03.php
@@ -52,7 +52,7 @@
             bien au travail.  Vous voulez-adapter l'intérieur de vos bureaux, de votre boutique et que ceux-ci 
             reflètent les valeurs de votre entreprise.</p><br>
         <p class="fenetreExplication">
-            <a href='#'><span onclick="fenetrePopup01()">Uppleva vous propose de travailler, avec vous, l'image de votre entreprise.</span></a>
+            <a href='#'><span onmouseover="fenetrePopup01()">Uppleva vous propose de travailler, avec vous, l'image de votre entreprise.</span></a>
         </p><br>
         <p>
             Mobilier sur mesure
@@ -105,76 +105,86 @@
 
 	<script type="text/javascript">
 		function fenetrePopup01() {
-							var myWindow = window.open("", "Uppleva - et votre entreprise", "width=700,height=600");
-							var txtA = '<div id="monImage001" style="background-image: url(../10images/ext003.jpg); ';
-							var txtA = txtA + 'height: 598px; ';
-							var txtA = txtA + 'background-position: center; ';
-							var txtA = txtA + 'background-repeat: no-repeat; ';
-							var txtA = txtA + 'background-size: cover; ';
-							var txtA = txtA + 'opacity: 0.7;';
-							var txtA = txtA + '">';
-							var txtA = txtA + '<div class="titreImage" style="opacity: 1;"';
-							var txtA = txtA + '>';
-							var txtA = txtA + '<ul class="texteSurPopup" style="font-size: 4vw; font-weight: bold; ';
-							var txtA = txtA + 'color: black; opacity: 1;"';
-							var txtA = txtA + '><li>';
-							var txtA = txtA + "Réalisation des metrés" + "</li><li>";
-							var txtA = txtA + "Plan 2D et 3D" + "</li><li>";
-							var txtA = txtA + "Recherches et études des matériaux, mobiliers, décorations" + "</li><li>";
-							var txtA = txtA + "Conseils couleur, éclairage, ambiance" + "</li><li>";
-							var txtA = txtA + "Etude de devis des corps de métier" + "</li><li>";
-							var txtA = txtA + "Partenariat avec des graphistes pour la réalisation de votre logo" + "</li><li>";
-							var txtA = txtA + "Suivi de chantier" + "</li>";
-							var txtA = txtA + "</ul></div></div>";
+                    var largeurWindow = window.screen.availWidth;
+                    var hauteurWindow = window.screen.availHeight;
+                    var myWindow = window.open("", "Uppleva - et votre entreprise", "width=largeurWindow,height=hauteurWindow, titlebar=no, location=yes, menubar=no, resizable=yes, status=yes");
+                    var txtA = '<div id="monImage001" style="background-image: url(../10images/ext003.jpg); ';
+                    var txtA = txtA + 'height: 598px; ';
+                    var txtA = txtA + 'background-position: center; ';
+                    var txtA = txtA + 'background-repeat: no-repeat; ';
+                    var txtA = txtA + 'background-size: cover; ';
+                    var txtA = txtA + 'opacity: 0.7;';
+                    var txtA = txtA + '">';
+                    var txtA = txtA + '<div class="titreImage" style="opacity: 1;"';
+                    var txtA = txtA + '>';
+                    var txtA = txtA + '<ul class="texteSurPopup" style="font-size: 4vw; font-weight: bold; ';
+                    var txtA = txtA + 'color: black; opacity: 1;"';
+                    var txtA = txtA + '><li>';
+                    var txtA = txtA + "Réalisation des metrés" + "</li><li>";
+                    var txtA = txtA + "Plan 2D et 3D" + "</li><li>";
+                    var txtA = txtA + "Recherches et études des matériaux, mobiliers, décorations" + "</li><li>";
+                    var txtA = txtA + "Conseils couleur, éclairage, ambiance" + "</li><li>";
+                    var txtA = txtA + "Etude de devis des corps de métier" + "</li><li>";
+                    var txtA = txtA + "Partenariat avec des graphistes pour la réalisation de votre logo" + "</li><li>";
+                    var txtA = txtA + "Suivi de chantier" + "</li>";
+                    var txtA = txtA + "</ul></div></div>";
 		myWindow.document.write("<p>" + myWindow.name + "</p>" + txtA);
 	};
 					
-					function fenetrePopup02() {
-							var myWindow = window.open("", "Uppleva - Le meuble dont vous rêvez", "width=600,height=300");
-							var txtB = "<ul><li>";
-							var txtB = txtB + "Plans 2D et 3D" + "</li><li>";
-							var txtB = txtB + "Recherches et études des matériaux adaptés" + "</li><li>";
-							var txtB = txtB + "Partenariat avec des menuisiers proposant un travail d'une grande qualité" + "</li>";
-							var txtB = txtB + "</ul>";
+		function fenetrePopup02() {
+                    var largeurWindow = window.screen.availWidth;
+                    var hauteurWindow = window.screen.availHeight;
+                    var myWindow = window.open("", "Uppleva - Le meuble dont vous rêvez", "width=largeurWindow,height=hauteurWindow, titlebar=no, location=yes, menubar=no, resizable=yes, status=yes");
+                    var txtB = "<ul><li>";
+                    var txtB = txtB + "Plans 2D et 3D" + "</li><li>";
+                    var txtB = txtB + "Recherches et études des matériaux adaptés" + "</li><li>";
+                    var txtB = txtB + "Partenariat avec des menuisiers proposant un travail d'une grande qualité" + "</li>";
+                    var txtB = txtB + "</ul>";
 		myWindow.document.write("<p>" + myWindow.name + "</p>" + txtB);
-					};
+	};
 					
 					
-					function fenetrePopup03() {
-						var myWindow = window.open("", "Uppleva - Projet de construction ou de rénovation", "width=600,height=300");
-						var txtC = "<ul><li>";
-						var txtC = txtC + "Réalisation des métrés" + "</li><li>";
-						var txtC = txtC + "Plans 2D ou 3D, selon vos besoins" + "</li><li>";
-						var txtC = txtC + "Recherches et études des matériaux, mobiliers, décorations" + "</li><li>";
-						var txtC = txtC + "Conseils couleur, éclairage, ambiance" + "</li><li>";
-						var txtC = txtC + "Etude de devis des corps de métier" + "</li><li>";
-						var txtC = txtC + "Partenaires de qualité" + "</li><li>";
-						var txtC = txtC + "Suivi de chantier" + "</li>";
-						var txtC = txtC + "</ul>";
-						myWindow.document.write("<p>" + myWindow.name + "</p>" + txtC);
-					};
+		function fenetrePopup03() {
+                    var largeurWindow = window.screen.availWidth;
+                    var hauteurWindow = window.screen.availHeight;
+                    var myWindow = window.open("", "Uppleva - Projet de construction ou de rénovation", "width=largeurWindow,height=hauteurWindow, titlebar=no, location=yes, menubar=no, resizable=yes, status=yes");
+                    var txtC = "<ul><li>";
+                    var txtC = txtC + "Réalisation des métrés" + "</li><li>";
+                    var txtC = txtC + "Plans 2D ou 3D, selon vos besoins" + "</li><li>";
+                    var txtC = txtC + "Recherches et études des matériaux, mobiliers, décorations" + "</li><li>";
+                    var txtC = txtC + "Conseils couleur, éclairage, ambiance" + "</li><li>";
+                    var txtC = txtC + "Etude de devis des corps de métier" + "</li><li>";
+                    var txtC = txtC + "Partenaires de qualité" + "</li><li>";
+                    var txtC = txtC + "Suivi de chantier" + "</li>";
+                    var txtC = txtC + "</ul>";
+                    myWindow.document.write("<p>" + myWindow.name + "</p>" + txtC);
+	};
 					
-					function fenetrePopup04() {
-						var myWindow = window.open("", "Uppleva - Un nouveau souffle à votre intérieur !", "width=600,height=300");
-						var txtD = "<ul><li>";
-						var txtD = txtD + "Recherche d'objets de décoration, de couleurs, de luminaires, de mobiliers" + "</li><li>";
-						var txtD = txtD + "Partenariats avec des marques scandinaves exceptionnelles" + "</li><li>";
-						var txtD = txtD + "Personal shopping" + "</li><li>";
-						var txtD = txtD + "Conseils, coaching" + "</li>";
-						var txtD = txtD + "" + "</ul>";
-						myWindow.document.write("<p>" + myWindow.name + "</p>" + txtD);
-					};
+		function fenetrePopup04() {
+                    var largeurWindow = window.screen.availWidth;
+                    var hauteurWindow = window.screen.availHeight;
+                    var myWindow = window.open("", "Uppleva - Un nouveau souffle à votre intérieur !", "width=largeurWindow,height=hauteurWindow, titlebar=no, location=yes, menubar=no, resizable=yes, status=yes");
+                    var txtD = "<ul><li>";
+                    var txtD = txtD + "Recherche d'objets de décoration, de couleurs, de luminaires, de mobiliers" + "</li><li>";
+                    var txtD = txtD + "Partenariats avec des marques scandinaves exceptionnelles" + "</li><li>";
+                    var txtD = txtD + "Personal shopping" + "</li><li>";
+                    var txtD = txtD + "Conseils, coaching" + "</li>";
+                    var txtD = txtD + "" + "</ul>";
+                    myWindow.document.write("<p>" + myWindow.name + "</p>" + txtD);
+	};
 					
-					function fenetrePopup05() {
-						var myWindow = window.open("", "Uppleva - Coaching", "width=600,height=300");
-						var txtE = "<ul><li>";
-						var txtE = txtE + "Home organising" + "</li><li>";
-						var txtE = txtE + "Home staging" + "</li><li>";
-						var txtE = txtE + "Visite &laquo; conseils &raquo;" + "</li><li>";
-						var txtE = txtE + "Conseils, coaching" + "</li>";
-						var txtE = txtE + "" + "</ul>";
-						myWindow.document.write("<p>" + myWindow.name + "</p>" + txtE);
-					};
+		function fenetrePopup05() {
+                    var largeurWindow = window.screen.availWidth;
+                    var hauteurWindow = window.screen.availHeight;
+                    var myWindow = window.open("", "Uppleva - Coaching", "width=largeurWindow,height=hauteurWindow, titlebar=no, location=yes, menubar=no, resizable=yes, status=yes");
+                    var txtE = "<ul><li>";
+                    var txtE = txtE + "Home organising" + "</li><li>";
+                    var txtE = txtE + "Home staging" + "</li><li>";
+                    var txtE = txtE + "Visite &laquo; conseils &raquo;" + "</li><li>";
+                    var txtE = txtE + "Conseils, coaching" + "</li>";
+                    var txtE = txtE + "" + "</ul>";
+                    myWindow.document.write("<p>" + myWindow.name + "</p>" + txtE);
+	};
 	</script>
 
 </body>
