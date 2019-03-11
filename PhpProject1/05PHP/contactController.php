12e37fb2c494cdda267c1a628c03b1a335cbba11
MarcBouchonville <marcbouchonville@gmail.com>
2019-03-11 13:26:22 +0100

    mise à jour table tvisiteurs reprise file php contactController

--- PhpProject1/05PHP/contactController.php
+++ PhpProject1/05PHP/contactController.php
@@ -1,91 +1,60 @@
-<?php 
+<?php
+
+    /*  formulaire page05.php rempli ?  */
 
     $valid = isset($_POST['Nom']) && isset($_POST['Email']);
     
     if ($valid) {
-        // si $valid N'est PAS NULL, alors ces data sont remplies
+        // si $valid n'est PAS NULL, alors ces data sont remplies
         // nettoyage et assignation
         
-        // puis nettoyage avec strip_tags pour retirer les problèmes de code
-        // PHP ou HTML encodé dans les data transmises !
-        
-        $nom01 = strip_tags(filter_input(INPUT_POST, 'Nom', FILTER_SANITIZE_STRING));
-        $nom = mb_strtoupper($nom01, 'UTF-8');
-        $prenom01 = strip_tags(filter_input(INPUT_POST, 'Prenom', FILTER_SANITIZE_STRING));
+        $nom01 = filter_input(INPUT_POST,'Nom',FILTER_SANITIZE_STRING);
+        $nom = mb_strtoupper($nom01,'UTF-8');
+        $prenom01 = filter_input(INPUT_POST,'Prenom',FILTER_SANITIZE_STRING);
         $prenom = ucwords($prenom01);
-        $email = strip_tags(filter_input(INPUT_POST, 'Email', FILTER_SANITIZE_EMAIL));
-        $telephone = strip_tags(filter_input(INPUT_POST, 'Telephone', FILTER_SANITIZE_STRING));
-        $sujet = strip_tags(filter_input(INPUT_POST, 'Sujet', FILTER_SANITIZE_STRING));
-        $commentaire = strip_tags(filter_input(INPUT_POST, 'Comment', FILTER_SANITIZE_STRING));
-        $acceptePerso = filter_input(INPUT_POST, 'acceptePerso', FILTER_VALIDATE_BOOLEAN);
+        $email = filter_input(INPUT_POST,'Email',FILTER_SANITIZE_EMAIL);
+        $telephone = filter_input(INPUT_POST,'Telephone',FILTER_SANITIZE_STRING);
+        $sujet = filter_input(INPUT_POST,'Sujet',FILTER_SANITIZE_STRING);
+        $commentaire = filter_input(INPUT_POST,'Comment',FILTER_SANITIZE_STRING);
+        
     }
     else
     {
-        header('Location: ../01pages/page07.php');
+        header('Location: ../pages/page05.php');
         exit();
     }
     
-    /*  debut traitement de renvoi vers adresse mail    */
-
-    // test de cette adresse pour éviter les spams
-    $Destinataire = "pitchhawthorn@gmail.com";
-    /*
-    if (!preg_match("#/^[a-z0-9._-]+@(hotmail\live\msn).[a-z]{2,4}$#/", $Destinataire)) {
-        $sautLigne = "\r\n";
+    
+    /*  debut traitement PHP    */
+    
+    require("connect.php");
+    try {
+        $pdo = new PDO("mysql:host=" . SERVER . ";dbname=" . BASE . ";charset=utf8", USER, PASSWD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+        
+        //  statements :
+        $stmt = $pdo->prepare("INSERT INTO TVisiteurs" 
+                . " (Nom, Prenom, Email, Telephone, Sujet, Commentaire)"
+                . " VALUES(?, ?, ?, ?, ?, ?)");
+        
+        $stmt->bindValue(1, $nom);
+        $stmt->bindValue(2, $prenom);
+        $stmt->bindValue(3, $email);
+        $stmt->bindValue(4, $telephone);
+        $stmt->bindValue(5, $sujet);
+        $stmt->bindValue(6, $commentaire);
+        
+        $stmt->execute();
+        
+        //  VERIFICATION :
+        session_start();
+        $_SESSION['tonNom'] = $_POST['nom'];
+        $_SESSION['tonPrenom'] = $_POST['prenom'];
+        echo "Vos coordonnées sont enregistrées <br> merci";
+        
+        header('Location: ../pages/page10.php');
     }
-    else {
-        $sautLigne = "\n";
+    catch(PDOException $e) {
+        echo "Erreur: " . $e;
     }
-    */
-    // le $sautLigne n'est utilisé QUE pour le $HEADER
-    // Dans le msg ($message), les lignes sont séparées par     \n    et NE peuvent excéder 70 caractères !
-    
-    $sautLigne = "\r\n";
-    //$message = 'message complet';
-
-
-    $sujetEnvoi = "mail1stTest";
-    $message = "Nom : " . $nom . "\n" . "Prenom : " . $prenom . "\n" . "Email : " . $email . "\n" . "Téléphone : " . $telephone . "\n" . "Sujet : " . $sujet . "\n" . "Commentaire : " . $commentaire . "\n" . "Accepté : " . $acceptePerso ;
-    $header = "From:pitchhawthorn19@gmail.com \r\n";
-    $header .= "Cc:pitchhawthorn19@gmail.com \r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-type: text/html\r\n";
-
-
-    $success = mail($Destinataire, $sujetEnvoi, $message);
-    if ($success == true) {
-        echo 'merci de nous avoir transmis vos coordonnées.<br>';
-        echo 'Votre réponse vous sera transmise dans les meilleurs délais<br>';
-        echo 'Bien à Vous ' . $nom . ' ' . $prenom;
-        // header('Location: ../01pages/page07.php');
-        exit();
-    } else {
-        $errorMessage = error_get_last()['message'];
-        header('Location: ../01pages/page07.php');
-        exit();
-    }
-    
-    // autre test de simple envoi de mail :
-    /*
-    $objet = "premier test";
-    $message = "message complet";
-    
-    mail($Destinataire, $objet, $message);
-    echo "lettre envoyée à ".$Destinataire;
-    */
-    
-    // ----------------------------------------------
-
-    /* 1er TEST avec problème : 
-    // construction de l'entête du mail
-    $header = "From: \"Uppleva\"<monsite@http://localhost:9010/>".$sautLigne;
-    $header .= "Reply-to: \"Uppleva\"<noreply@http://localhost:9010/>".$sautLigne;
-    $objet = "premier test d'envoi de msg pour fichier uppleva";
-    $message = "Nom : " . $nom . $sautLigne . "Prenom : " . $prenom . $sautLigne . "Email : " . $email . $sautLigne . "Téléphone : " . $telephone . $sautLigne . "Sujet : " . $sujet . sautLigne . "Commentaire : " . $commentaire . sautLigne . "Accepté : " . acceptePerso ;
-    if (filter_var($Destinataire, FILTER_VALIDATE_EMAIL)) {
-        mail($Destinataire, $objet, $$message, $header);
-    }
-    
-     */
-    
 ?>
---
+++ PhpProject1/05PHP/contactControllerTest_Old.php
@@ -0,0 +1,91 @@
+<?php 
+
+    $valid = isset($_POST['Nom']) && isset($_POST['Email']);
+    
+    if ($valid) {
+        // si $valid N'est PAS NULL, alors ces data sont remplies
+        // nettoyage et assignation
+        
+        // puis nettoyage avec strip_tags pour retirer les problèmes de code
+        // PHP ou HTML encodé dans les data transmises !
+        
+        $nom01 = strip_tags(filter_input(INPUT_POST, 'Nom', FILTER_SANITIZE_STRING));
+        $nom = mb_strtoupper($nom01, 'UTF-8');
+        $prenom01 = strip_tags(filter_input(INPUT_POST, 'Prenom', FILTER_SANITIZE_STRING));
+        $prenom = ucwords($prenom01);
+        $email = strip_tags(filter_input(INPUT_POST, 'Email', FILTER_SANITIZE_EMAIL));
+        $telephone = strip_tags(filter_input(INPUT_POST, 'Telephone', FILTER_SANITIZE_STRING));
+        $sujet = strip_tags(filter_input(INPUT_POST, 'Sujet', FILTER_SANITIZE_STRING));
+        $commentaire = strip_tags(filter_input(INPUT_POST, 'Comment', FILTER_SANITIZE_STRING));
+        $acceptePerso = filter_input(INPUT_POST, 'acceptePerso', FILTER_VALIDATE_BOOLEAN);
+    }
+    else
+    {
+        header('Location: ../01pages/page07.php');
+        exit();
+    }
+    
+    /*  debut traitement de renvoi vers adresse mail    */
+
+    // test de cette adresse pour éviter les spams
+    $Destinataire = "pitchhawthorn@gmail.com";
+    /*
+    if (!preg_match("#/^[a-z0-9._-]+@(hotmail\live\msn).[a-z]{2,4}$#/", $Destinataire)) {
+        $sautLigne = "\r\n";
+    }
+    else {
+        $sautLigne = "\n";
+    }
+    */
+    // le $sautLigne n'est utilisé QUE pour le $HEADER
+    // Dans le msg ($message), les lignes sont séparées par     \n    et NE peuvent excéder 70 caractères !
+    
+    $sautLigne = "\r\n";
+    //$message = 'message complet';
+
+
+    $sujetEnvoi = "mail1stTest";
+    $message = "Nom : " . $nom . "\n" . "Prenom : " . $prenom . "\n" . "Email : " . $email . "\n" . "Téléphone : " . $telephone . "\n" . "Sujet : " . $sujet . "\n" . "Commentaire : " . $commentaire . "\n" . "Accepté : " . $acceptePerso ;
+    $header = "From:pitchhawthorn19@gmail.com \r\n";
+    $header .= "Cc:pitchhawthorn19@gmail.com \r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
+
+
+    $success = mail($Destinataire, $sujetEnvoi, $message);
+    if ($success == true) {
+        echo 'merci de nous avoir transmis vos coordonnées.<br>';
+        echo 'Votre réponse vous sera transmise dans les meilleurs délais<br>';
+        echo 'Bien à Vous ' . $nom . ' ' . $prenom;
+        // header('Location: ../01pages/page07.php');
+        exit();
+    } else {
+        $errorMessage = error_get_last()['message'];
+        header('Location: ../01pages/page07.php');
+        exit();
+    }
+    
+    // autre test de simple envoi de mail :
+    /*
+    $objet = "premier test";
+    $message = "message complet";
+    
+    mail($Destinataire, $objet, $message);
+    echo "lettre envoyée à ".$Destinataire;
+    */
+    
+    // ----------------------------------------------
+
+    /* 1er TEST avec problème : 
+    // construction de l'entête du mail
+    $header = "From: \"Uppleva\"<monsite@http://localhost:9010/>".$sautLigne;
+    $header .= "Reply-to: \"Uppleva\"<noreply@http://localhost:9010/>".$sautLigne;
+    $objet = "premier test d'envoi de msg pour fichier uppleva";
+    $message = "Nom : " . $nom . $sautLigne . "Prenom : " . $prenom . $sautLigne . "Email : " . $email . $sautLigne . "Téléphone : " . $telephone . $sautLigne . "Sujet : " . $sujet . sautLigne . "Commentaire : " . $commentaire . sautLigne . "Accepté : " . acceptePerso ;
+    if (filter_var($Destinataire, FILTER_VALIDATE_EMAIL)) {
+        mail($Destinataire, $objet, $$message, $header);
+    }
+    
+     */
+    
+?>
